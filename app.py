7d2e5fb2f43cd86ff79744f4226120fc50c34598
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-03 19:37:13 +0100

    Add follow and unfollow buttons

--- app.py
+++ app.py
@@ -104,6 +104,44 @@ def post():
         return redirect(url_for('index'))
     return render_template('post.html', form=form)
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    try:
+        to_user = models.User.get(models.User.username**username)
+    except models.DoesNotExist:
+        pass
+    else:
+        try:
+            models.Relationship.create(
+                from_user=g.user._get_current_object(),
+                to_user=to_user
+            )
+        except models.IntegrityError:
+            pass
+        else:
+            flash("You're now following {}!".format(to_user.username), "success")
+    return redirect(url_for('stream', username=to_user.username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    try:
+        to_user = models.User.get(models.User.username**username)
+    except models.DoesNotExist:
+        pass
+    else:
+        try:
+            models.Relationship.get(
+                from_user=g.user._get_current_object(),
+                to_user=to_user
+            ).delete_instance()
+        except models.IntegrityError:
+            pass
+        else:
+            flash("You've unfollowed {}!".format(to_user.username), "success")
+    return redirect(url_for('stream', username=to_user.username))
+
 
 if __name__ == '__main__':
     models.initialize()
